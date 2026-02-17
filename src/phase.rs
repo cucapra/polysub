a1167d070e332b8f2a73b71f2638b944eba991ee
Kevin Läufer <laeufer@cornell.edu>
2026-02-17 12:20:59 -0500

    phase: test inversion

--- src/phase.rs
+++ src/phase.rs
@@ -3,6 +3,7 @@
 // author: Kevin Laeufer <laeufer@cornell.edu>
 
 use crate::{Coef, Mod, Polynom, Term, VarIndex};
+use std::fmt::{Display, Formatter};
 
 /// Represent a polynomial which internally uses the phase change optimization from the
 /// FMCAD'24 paper: "Symbolic Computer Algebra for Multipliers Revisited - It's All About Orders and Phases"
@@ -16,22 +17,15 @@ pub struct PhaseOptPolynom<C: Coef> {
 
 impl<C: Coef> PhaseOptPolynom<C> {
     pub fn new(m: Mod) -> Self {
-        let inner = Polynom::new(m);
-        let inverted = BitSet::new();
-        Self { inner, inverted }
+        Polynom::new(m).into()
     }
 
     pub fn with_max_mod() -> Self {
-        let inner = Polynom::with_max_mod();
-        let inverted = BitSet::new();
-        Self { inner, inverted }
+        Polynom::with_max_mod().into()
     }
 
     pub fn from_monoms(m: Mod, monoms: impl Iterator<Item = (C, Term)>) -> Self {
-        let inner = Polynom::from_monoms(m, monoms);
-        // by default, no variables are inverted!
-        let inverted = BitSet::new();
-        Self { inner, inverted }
+        Polynom::from_monoms(m, monoms).into()
     }
 }
 
@@ -83,23 +77,47 @@ impl<C: Coef> PhaseOptPolynom<C> {
 
 /// Routines to replace specific gate types
 impl<C: Coef> PhaseOptPolynom<C> {
+    /// out = a && !b
+    pub fn replace_and_not(&mut self, out: VarIndex, a: VarIndex, b: VarIndex) {
+        self.ensure_not_inverted(out);
+        match (self.is_inverted(a), !self.is_inverted(b)) {
+            (false, false) => self.inner.replace_and(out, a, b),
+            (true, false) => self.replace_and_one_inverted(out, b, a),
+            (false, true) => self.replace_and_one_inverted(out, a, b),
+            (true, true) => self.replace_and_both_inverted(out, a, b),
+        }
+    }
+
+    /// out = !a && !b
+    pub fn replace_and_not_not(&mut self, out: VarIndex, a: VarIndex, b: VarIndex) {
+        self.ensure_not_inverted(out);
+        match (!self.is_inverted(a), !self.is_inverted(b)) {
+            (false, false) => self.inner.replace_and(out, a, b),
+            (true, false) => self.replace_and_one_inverted(out, b, a),
+            (false, true) => self.replace_and_one_inverted(out, a, b),
+            (true, true) => self.replace_and_both_inverted(out, a, b),
+        }
+    }
+
     pub fn replace_and(&mut self, out: VarIndex, a: VarIndex, b: VarIndex) {
         self.ensure_not_inverted(out);
         match (self.is_inverted(a), self.is_inverted(b)) {
             (false, false) => self.inner.replace_and(out, a, b),
             (true, false) => self.replace_and_one_inverted(out, b, a),
             (false, true) => self.replace_and_one_inverted(out, a, b),
-            (true, true) => {
-                // 1 - !a - !b + !a!b
-                let monoms = [
-                    (C::from_i64(1, self.get_mod()), vec![].into()),
-                    (C::from_i64(-1, self.get_mod()), vec![a].into()),
-                    (C::from_i64(-1, self.get_mod()), vec![b].into()),
-                    (C::from_i64(1, self.get_mod()), vec![a, b].into()),
-                ];
-                self.inner.replace_var(out, &monoms);
-            }
+            (true, true) => self.replace_and_both_inverted(out, a, b),
         }
+    }
+
+    fn replace_and_both_inverted(&mut self, out: VarIndex, a: VarIndex, b: VarIndex) {
+        // 1 - !a - !b + !a!b
+        let monoms = [
+            (C::from_i64(1, self.get_mod()), vec![].into()),
+            (C::from_i64(-1, self.get_mod()), vec![a].into()),
+            (C::from_i64(-1, self.get_mod()), vec![b].into()),
+            (C::from_i64(1, self.get_mod()), vec![a, b].into()),
+        ];
+        self.inner.replace_var(out, &monoms);
     }
 
     fn replace_and_one_inverted(&mut self, out: VarIndex, normal: VarIndex, inverted: VarIndex) {
@@ -120,14 +138,7 @@ impl<C: Coef> PhaseOptPolynom<C> {
             (false, false) => self.inner.replace_or(out, a, b),
             (true, false) => self.replace_or_one_inverted(out, b, a),
             (false, true) => self.replace_or_one_inverted(out, a, b),
-            (true, true) => {
-                // 1 - !a!b
-                let monoms = [
-                    (C::from_i64(1, self.get_mod()), vec![].into()),
-                    (C::from_i64(-1, self.get_mod()), vec![a, b].into()),
-                ];
-                self.inner.replace_var(out, &monoms);
-            }
+            (true, true) => self.replace_or_both_inverted(out, a, b),
         }
     }
 
@@ -140,6 +151,15 @@ impl<C: Coef> PhaseOptPolynom<C> {
                 C::from_i64(1, self.get_mod()),
                 vec![normal, inverted].into(),
             ),
+        ];
+        self.inner.replace_var(out, &monoms);
+    }
+
+    fn replace_or_both_inverted(&mut self, out: VarIndex, a: VarIndex, b: VarIndex) {
+        // 1 - !a!b
+        let monoms = [
+            (C::from_i64(1, self.get_mod()), vec![].into()),
+            (C::from_i64(-1, self.get_mod()), vec![a, b].into()),
         ];
         self.inner.replace_var(out, &monoms);
     }
@@ -180,6 +200,16 @@ impl<C: Coef> PhaseOptPolynom<C> {
         }
     }
 
+    pub fn replace_identity(&mut self, out: VarIndex, a: VarIndex) {
+        if self.is_inverted(out) == self.is_inverted(a) {
+            // same phase
+            self.inner.replace_identity(out, a);
+        } else {
+            // inverted phases, need to invert
+            self.inner.replace_not(out, a);
+        }
+    }
+
     pub fn replace_true(&mut self, out: VarIndex) {
         if self.is_inverted(out) {
             self.inner.replace_false(out);
@@ -193,6 +223,52 @@ impl<C: Coef> PhaseOptPolynom<C> {
             self.inner.replace_true(out);
         } else {
             self.inner.replace_false(out);
+        }
+    }
+}
+
+impl<C: Coef> From<PhaseOptPolynom<C>> for Polynom<C> {
+    fn from(mut value: PhaseOptPolynom<C>) -> Self {
+        // first we need to make sure that all variables are not inverted
+        let inverted_vars: Vec<_> = value
+            .inner
+            .iter_vars()
+            .filter(|v| value.is_inverted(*v))
+            .collect();
+        for var in inverted_vars {
+            value.invert(var);
+        }
+        value.inner
+    }
+}
+
+impl<C: Coef + Display> Display for PhaseOptPolynom<C> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.is_zero() {
+            write!(f, "0 (empty polynomial)")
+        } else {
+            let monoms = self.inner.sorted_monom_vec();
+            for (ii, (coef, term)) in monoms.iter().enumerate() {
+                let is_last = ii == monoms.len() - 1;
+                write!(f, "[{coef}*")?;
+                for (iv, &var) in term.vars().enumerate() {
+                    let is_first = iv == 0;
+                    if !is_first {
+                        write!(f, "*")?;
+                    }
+                    if self.is_inverted(var) {
+                        write!(f, "i{}", u32::from(var))?;
+                    } else {
+                        write!(f, "x{}", u32::from(var))?;
+                    }
+                }
+                if !is_last {
+                    write!(f, "] + ")?;
+                } else {
+                    write!(f, "]")?;
+                }
+            }
+            Ok(())
         }
     }
 }
@@ -228,5 +304,37 @@ impl BitSet {
         } else {
             (self.words[word_index] >> bit) & 1 == 1
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_invert() {
+        // example 1 from the FMCAD'24 paper: x1 || x2 || x3
+        let poly_str = "1*x1 + 1*x2 + 1*x3 - 1*x1*x2 - 1*x1*x3 - 1*x2*x3 + 1*x1*x2*x3";
+        let mut poly: PhaseOptPolynom<u64> = Polynom::from_str(poly_str).unwrap().into();
+        assert_eq!(
+            format!("{poly}"),
+            "[1*x1] + [1*x2] + [1*x3] + [18446744073709551615*x1*x2] + [18446744073709551615*x1*x3] + [18446744073709551615*x2*x3] + [1*x1*x2*x3]"
+        );
+        poly.invert(1.into());
+        // 1 - !x1 + !x1x2 + !x1x3 - !x1x2x3
+        assert_eq!(
+            format!("{poly}"),
+            "[1*] + [18446744073709551615*i1] + [1*i1*x2] + [1*i1*x3] + [18446744073709551615*i1*x2*x3]"
+        );
+        poly.invert(2.into());
+        // 1 - !x1!x2 + !x1!x2x3
+        assert_eq!(
+            format!("{poly}"),
+            "[1*] + [18446744073709551615*i1*i2] + [1*i1*i2*x3]"
+        );
+        poly.invert(3.into());
+        // 1 - !x1!x2!x3
+        assert_eq!(format!("{poly}"), "[1*] + [18446744073709551615*i1*i2*i3]");
     }
 }
